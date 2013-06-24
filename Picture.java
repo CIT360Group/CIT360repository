740d55512675bf578b50d9845e2fb0c6bfe606a6
chrisc86 <chrisc86@gmail.com>
2013-06-24 12:26:24 -0500

    Create Picture.java

---
+++ Picture.java
@@ -0,0 +1,38 @@
+package com.example.mathapp;
+import java.util.ArrayList;
+
+
+public class Picture {
+
+
+   private Integer id;
+   private String src;
+   private Integer number;
+   
+   private Picture() {}
+   
+   public Picture(String src, int number) {
+      this.src = src;
+      this.number = number;
+   }
+   
+   public String toString() {
+       return "Picture [id=" + id + ", src=" + src + ", number=" + number + "]";
+   }
+   
+   public Integer getId() {
+      return id;
+   }
+   private void setId(Integer id) {
+      this.id = id;
+   }
+   public String getSrc() {
+      return src;
+   }
+   public void setText(String src) {
+      this.src = src;
+   }
+   public Integer getNumber() {
+      return number;
+   }
+}
